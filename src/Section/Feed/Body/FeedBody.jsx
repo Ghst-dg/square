303f699bd1608620e5b0a62f918d247549513208
Priyanshu Verma <gh0stm0nkey@proton.me>
2023-05-20 12:41:31 +0530

    feed body made

---
+++ src/Section/Feed/Body/FeedBody.jsx
@@ -0,0 +1,7 @@
+const FeedBody = () => {
+    return (
+        <div className="FeedBody"></div>
+    );
+}
+
+export default FeedBody;
--- src/Section/Feed/Feed.jsx
+++ src/Section/Feed/Feed.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../../Components/Navbar/Navbar';
+import FeedBody from './Body/FeedBody';
 import './Feed.css'
 
 const Feed = () => {
     return (
         <div className="Feed">
             <Navbar/>
+            <FeedBody/>
         </div>
     );
 }
